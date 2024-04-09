9078b9642d7eb9a38a1f4f1d6278a19fe4696cee
Abhiroop Kaur <abhiroopk.o@gmail.com>
2024-04-09 13:12:06 -0600

    adjusted the picture

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -8,13 +8,13 @@ const HeroBanner = () => {
   return (
     <Box
       sx={{
-        mt: { lg: '212px', xs: '70px' },
-        ml: { sm: '50px' },
+        mt: { lg: '270px', xs: '100px' },
+        ml: { sm: '30px' },
         position: 'relative',
-        p: '20px',
+        p: '10px',
       }}
     >
-      <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+      <Typography color="#FF2625" fontWeight="0" fontSize="26px">
         FitTracker
       </Typography>
       <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', sm: '40px' } }} mb="23px" mt="30px">
