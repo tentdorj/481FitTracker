afd43693286b1b53395670feec18111bc9dab301
tentdorj <144164843+tentdorj@users.noreply.github.com>
2024-04-09 23:07:29 -0600

    refactor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './App.css';
 import {Route, Routes} from 'react-router-dom';
 import {Box} from '@mui/material';
-import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -10,16 +9,17 @@ import Nutrition from './pages/Nutrition';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 
+
 const App = () => {
   return (
     <Box width="400px" sx={{width: {xl: '1488px'}}} m= "auto">
     <Navbar />
     <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/exercise/:id" element={<ExerciseDetail />} />
         <Route path ="/nutrition" element={<Nutrition/>}/>
         <Route path ="/signup" element={<Signup/>}/>
         <Route path ="/login" element={<Login/>}/>
+        
 
     </Routes>
     <Footer />
